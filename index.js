7a7202311cb1aa105302c9bcd0370f1782401055
Gabriel Mejia <gabrielm29929@gmail.com>
2022-02-01 19:41:37 -0800

    added weekly code

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ let annualExpenses = 0
 // Your Code Here
 
 window.prompt(weeklyExpenseQuestions[0])
+let answer = window.prompt (weeklyExpenseQuestions[0])
+console.log(answer)
+
+for(let i = 0; i < weeklyExpenseQuestions.length; i++){
+let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
+weeklyExpenses = weeklyExpenses + answer
+}
